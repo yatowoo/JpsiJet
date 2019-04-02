1971ded996119cb230486b5975425b0dcd2dfe2a
ytwu <torrence@mail.ustc.edu.cn>
2019-04-03 00:10:11 +0800

    QA -Add task for jet QA

--- runAnalysis.C
+++ runAnalysis.C
@@ -16,6 +16,13 @@ void runAnalysis(){
   AliAODInputHandler *aodH = new AliAODInputHandler();
   mgr->SetInputEventHandler(aodH);
 
+  // Task - Jet finder for charged jet
+  AliEmcalJetTask *pChJet02Task = AliEmcalJetTask::AddTaskEmcalJet("usedefault", "", AliJetContainer::antikt_algorithm, 0.2, AliJetContainer::kChargedJet, 0.15, 0, 0.01, AliJetContainer::pt_scheme, "Jet", 1., kFALSE, kFALSE);
+  pChJet02Task->SelectCollisionCandidates(AliVEvent::kAny);
+  // Task - PWGJE QA (Event, Track, Calo, Jet)
+  AliAnalysisTaskPWGJEQA* jetQA = reinterpret_cast<AliAnalysisTaskPWGJEQA*>(gInterpreter->ExecuteMacro("$ALICE_PHYSICS/PWGJE/EMCALJetTasks/macros/AddTaskPWGJEQA.C(\"usedefault\",\"usedefault\",\"usedefault\",\"\")"));
+  AliJetContainer* jetCont02 = jetQA->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.2, AliEmcalJet::kTPCfid, "Jet");
+
   // Task - PID QA
   gInterpreter->ExecuteMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
   gInterpreter->ExecuteMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDqa.C");
