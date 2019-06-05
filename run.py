2e5820f9f56a15b90180b33198f070101e003d91
ytwu <torrence@mail.ustc.edu.cn>
2019-06-05 09:28:26 +0200

    JOB - Add script for running

---
+++ run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+import sys
+import os
+
+CONFIG_FILE = "Job/AlienJob.json"
+with open(CONFIG_FILE) as f:
+  CONFIG = json.load(f)
+  print("[-] INFO - Read configuration file: " + CONFIG_FILE)
+  print(CONFIG.keys())
+
+JOB_NAME = sys.argv[1]
+JOB_MODE = sys.argv[2]
+
+doMult = int(CONFIG[JOB_NAME]["doMult"])
+doEmcalCorrection = int(CONFIG[JOB_NAME]["doEmcalCorrection"])
+doJetQA = int(CONFIG[JOB_NAME]["doJetQA"])
+doJpsiQA = int(CONFIG[JOB_NAME]["doJpsiQA"])
+doJpsiFilter = int(CONFIG[JOB_NAME]["doJpsiFilter"])
+doPIDQA = int(CONFIG[JOB_NAME]["doPIDQA"])
+
+cmd = "aliroot -b -l -q -x \'runAnalysis.C(" \
+    + repr(doMult) + ',' \
+    + repr(doEmcalCorrection) + ',' \
+    + repr(doJetQA) + ',' \
+    + repr(doJpsiQA) + ',' \
+    + repr(doJpsiFilter) + ',' \
+    + repr(doPIDQA) + ',' \
+    + '"' + JOB_MODE + '",' \
+    + '"' + CONFIG[JOB_NAME]["datasets"] + '",' \
+    + '"' + CONFIG[JOB_NAME]["data_dir"] + '",' \
+    + '"' + CONFIG[JOB_NAME]["work_dir"] + '",' \
+    + '"' + CONFIG[JOB_NAME]["task_name"] + '"' \
+    + ")\'"
+
+print("[-] INFO - Read job configuration: " + JOB_NAME)
+print(CONFIG[JOB_NAME])
+print("[-] INFO - Job running mode : " + JOB_MODE) 
+print("------> " + cmd)
+
+os.system(cmd)
