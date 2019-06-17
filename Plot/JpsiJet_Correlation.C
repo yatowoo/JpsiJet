8fd474d18d0875fc6e9f9a09caa1951f6ae006b1
ytwu <torrence@mail.ustc.edu.cn>
2019-06-17 21:05:07 +0800

    QA/Plot - Add sideband substraction

--- Plot/JpsiJet_Correlation.C
+++ Plot/JpsiJet_Correlation.C
@@ -5,12 +5,33 @@
  * Input: AliAOD.Dielectron.root
  * */
 
-Bool_t Accept(AliDielectronPair* jpsi, Bool_t reqPrompt = kFALSE){
+TH1* SubsctractSideband(TH1* hTotal, TH1* hSideband){
+
+  TH1* h = (TH1*)(hTotal->Clone("hCorr"));
+  h->Reset();
+  for(Int_t i = i; i <= hTotal->GetNbinsX(); i++){
+    for(Int_t j = 0; j <= hTotal->GetNbinsY(); j++){
+      Double_t nTotal = hTotal->GetBinContent(i,j);
+      Double_t nSB = hSideband->GetBinContent(i,j);
+      if(nTotal < 1.) continue;
+      h->SetBinContent(i,j, nTotal - nSB);
+    }
+  }
+  return h;
+}
+
+Bool_t Accept(AliDielectronPair* jpsi, Bool_t reqSideBand = kFALSE, Bool_t reqPrompt = kFALSE){
  
   Double_t invMass = jpsi->M();
   // pT cut - with EG2 threshold
   if(jpsi->Pt() < 5.0) return kFALSE;
-  if(invMass < 2.92 || invMass > 3.16)
+  Double_t Mmin = 2.92;
+  Double_t Mmax = 3.16;
+  if(reqSideBand){
+    Mmin = 3.24;
+    Mmax = 3.56;
+  }
+  if(invMass < Mmin || invMass > Mmax)
     return kFALSE;
   if(reqPrompt){
     return kFALSE;
@@ -50,13 +71,20 @@ void JpsiJet_Correlation(
   TH2D* hMap = new TH2D("hMap","J/#psi-Jet correlation function (#Delta#eta-#Delta#phi);#Delta#eta;#Delta#phi (rad);#entries",
       40, -2, 2,
       100, -4, 6);
+  TH2D* hMapSB = new TH2D("hMapSB","J/#psi-Jet (sideband) correlation function (#Delta#eta-#Delta#phi);#Delta#eta;#Delta#phi (rad);#entries",
+      40, -2, 2,
+      100, -4, 6);
   TH2D* hJpsiMap = new TH2D("hJpsi","J/#psi #eta-#phi distribution;#Delta#eta;#Delta#phi (rad);#entries",
+      20,-1,1,
+      100, -2, 8);
+  TH2D* hJpsiMapSB = new TH2D("hJpsiSB","J/#psi #eta-#phi distribution (sideband);#Delta#eta;#Delta#phi (rad);#entries",
       20,-1,1,
       100, -2, 8);
   TH2D* hJetMap = new TH2D("hJet","Jet #eta-#phi distribution;#Delta#eta;#Delta#phi (rad);#entries",
       50,-1,1,
       100, -2, 8);
   Int_t nJpsi = 0;
+  Int_t nJpsiSB = 0;
   for(Int_t i = 0 ; i < aodTree->GetEntries(); i++){
     aodTree->GetEntry(i);
     if(i % (aodTree->GetEntries() / 100) == 0)
@@ -64,19 +92,28 @@ void JpsiJet_Correlation(
     TIter nextPair(pairs);
     AliDielectronPair* jpsi = NULL;
     while(jpsi = static_cast<AliDielectronPair*>(nextPair())){
-      if(!Accept(jpsi)) continue;
+      Bool_t inSideBand = Accept(jpsi, kTRUE);
+      if(!Accept(jpsi) && !inSideBand) continue;
       Double_t jpsiPhi = TVector2::Phi_0_2pi(jpsi->Phi());
-      cout << "[+] J/psi found (Total: "  << ++nJpsi << "): " 
-        << jpsi->M() << ", " << jpsi->Pt() << ", " << jpsi->Eta() << ", " << jpsiPhi << endl;
-      hJpsiMap->Fill(jpsi->Eta(),jpsiPhi);
+      if(inSideBand){
+        cout << "[+] In sideband found (TotalSB: "  << ++nJpsiSB << "): " 
+          << jpsi->M() << ", " << jpsi->Pt() << ", " << jpsi->Eta() << ", " << jpsiPhi << endl;
+        hJpsiMapSB->Fill(jpsi->Eta(),jpsiPhi);
+      }
+      else{
+        cout << "[+] J/psi found (Total: "  << ++nJpsi << "): " 
+          << jpsi->M() << ", " << jpsi->Pt() << ", " << jpsi->Eta() << ", " << jpsiPhi << endl;
+        hJpsiMap->Fill(jpsi->Eta(),jpsiPhi);
+      }
       TIter nextJet(jets);
       AliEmcalJet* jet = NULL;
       while(jet = static_cast<AliEmcalJet*>(nextJet())){
         if(!Accept(jet)) continue;
         hJetMap->Fill(jet->Eta(),jet->Phi());
-        hMap->Fill(
-          jpsi->Eta() - jet->Eta(),
-          DeltaPhi(jpsiPhi,jet->Phi()));
+        if(inSideBand)
+          hMapSB->Fill(jpsi->Eta() - jet->Eta(), DeltaPhi(jpsiPhi,jet->Phi()));
+        else
+          hMap->Fill(jpsi->Eta() - jet->Eta(), DeltaPhi(jpsiPhi,jet->Phi()));
       }// Loop - jets
     }// Loop - pairs
   }// Loop - events
