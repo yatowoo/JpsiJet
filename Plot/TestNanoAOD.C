940ab7524b4d1f4877b5e83b1d766f22c37b8c91
ytwu <torrence@mail.ustc.edu.cn>
2019-06-26 00:25:22 +0800

    NanoAOD - Find pair daughters in track array

--- Plot/TestNanoAOD.C
+++ Plot/TestNanoAOD.C
@@ -2,36 +2,61 @@
 //
 
 TFile* f = NULL;
-TTree* aodTree = NULL;
+TTree* aodT = NULL;
 AliAODEvent* aod = NULL;
 TClonesArray* pairs = NULL;
 TClonesArray* daughters = NULL;
 
-void TestNanoAOD(const char* fileName = "AliAOD.Dielectron_strict.root"){
+Bool_t FindDaughters(AliVTrack* trk){
+  static const Double_t ERR_LIMIT = 1e-6;
+  TIter nextEle(daughters);
+  TLorentzVector* vec = NULL;
+  while(vec = static_cast<TLorentzVector*>(nextEle())){
+    if(TMath::Abs(vec->Pt() - trk->Pt()) < ERR_LIMIT &&
+       TMath::Abs(vec->Eta() - trk->Eta()) < ERR_LIMIT &&
+       TMath::Abs(TVector2::Phi_0_2pi(vec->Phi()) - trk->Phi()) < ERR_LIMIT )
+      return kTRUE;
+  }
+  return kFALSE;
+}
+
+void TestNanoAOD(const char* fileName = "AliAOD.ANA.root"){
 
   f = new TFile(fileName);
-  aodTree = (TTree*)(f->Get("aodTree"));
+  aodT = (TTree*)(f->Get("aodTree"));
   aod = new AliAODEvent;
   aod->ReadFromTree(aodTree);
   
   pairs = new TClonesArray("AliDielectronPair",100);
   aodTree->SetBranchAddress("dielectrons",&pairs);
 
-  daughters = new TClonesArray("AliKFParticle",100);
+  daughters = new TClonesArray("TLorentzVector",100);
 
-  for(Int_t i = 0 ; i < aodTree->GetEntries(); i++){
-    aodTree->GetEntry(i);
+  for(Int_t i = 0 ; i < aodT->GetEntries(); i++){
+    aodT->GetEntry(i);
     TIter nextPair(pairs);
     AliDielectronPair* jpsi = NULL;
     daughters->Clear("C");
-    Int_t nEle = daughters->GetEntries();
+    Int_t nEle = 0;
     while(jpsi = static_cast<AliDielectronPair*>(nextPair())){
       AliKFParticle& ele = jpsi->GetKFFirstDaughter();
-      //new ((*daughters)[nEle++]) AliKFParticle(ele);
-      cout << ele.GetPt() << "  " << ele.GetEta() << "  " << ele.GetPhi() << endl;
+      new ((*daughters)[nEle++]) TLorentzVector(ele.GetPx(),ele.GetPy(),ele.GetPz(),ele.GetE());
       ele = jpsi->GetKFSecondDaughter();
-      //new ((*daughters)[nEle++]) AliKFParticle(ele);
-      cout << ele.GetPt() << "  " << ele.GetEta() << "  " << ele.GetPhi() << endl;
+      new ((*daughters)[nEle++]) TLorentzVector(ele.GetPx(),ele.GetPy(),ele.GetPz(),ele.GetE());
+    }
+    if(nEle == 0) continue;
+    cout << "[-] Event " << i << " - Pairs=" << pairs->GetEntries()
+     << " Daughters=" << daughters->GetEntries() << endl; 
+    TIter nextTrack(aod->GetTracks());
+    AliAODTrack* trk = NULL;
+    Int_t nTrackMatched = 0;
+    while(trk = static_cast<AliAODTrack*>(nextTrack())){
+      if(FindDaughters(trk)) nTrackMatched++; 
+    }
+    if(nEle != nTrackMatched){
+      cout << "[X] ERROR - event " << i << " : nTrackMatched=" << nTrackMatched << endl;
+      daughters->Print();
+      //exit(1);
     }
   }
 }
