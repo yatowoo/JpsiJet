ba98dbded314ba0ae3eb1f275636950e9864e79e
ytwu <torrence@mail.ustc.edu.cn>
2019-09-16 14:11:16 +0800

    Plot - Unfolding re-norm response by column

--- Plot/Unfold.py
+++ Plot/Unfold.py
@@ -2,13 +2,12 @@
 # Test for RooUnfold
 
 import ROOT
-from ROOT import TFile, TCanvas
+from ROOT import TFile, TCanvas, TLegend
 from ROOT import RooUnfold, RooUnfoldBayes, RooUnfoldSvd, RooUnfoldResponse
-from ana_util import HistNorm, SelectColor, SelectMarker
+import ana_util
+from ana_util import ResponseNorm, SelectColor, SelectMarker
 
 # Drawing
-COLOR = SelectColor()
-MARKER = SelectMarker()
 padFF = TCanvas('cFF','Unfolding', 1600, 600)
 padFF.Divide(2)
 padFF.Draw()
@@ -18,35 +17,49 @@ raw = TFile('../JpsiJetAna.root')
 # MC outputs
 mc = TFile('../output/QM19/JpsiJetMC.root')
 
+def DrawFF(hist, name):
+  hist.SetName(name)
+  hist.Scale(1./hist.Integral('width'))
+  histColor = next(ana_util.COLOR)
+  hist.SetLineColor(histColor)
+  hist.SetMarkerColor(histColor)
+  hist.SetMarkerStyle(next(ana_util.MARKER))
+  hist.Draw('same PE')
+  return hist
+
 def UnfoldFF(rawFF, detResponse, tag):
-  # Z measured
-  rawFF.SetBins(10, 0, 1.0)
-  rawFF.Scale(1./rawFF.Integral('width'))
-  rawFF.Draw()
+  padFF.Clear()
+  padFF.Divide(2)
   # Detector response
-  detResponse.Scale(1./detResponse.Integral())
+  ResponseNorm(detResponse)
+  padFF.cd(1)
+  detResponse.Draw('COLZ')
   response = RooUnfoldResponse(rawFF, rawFF, detResponse)
   bayes = RooUnfoldBayes(response, rawFF)
   # Unfolding - Bayesian
-  COLOR = SelectColor()
-  MARKER = SelectMarker()
+  ana_util.COLOR = SelectColor(0)
+  ana_util.MARKER = SelectMarker(0)
+  padFF.cd(2)
+  # Legend
+  lgd = TLegend(0.12, 0.6, 0.3, 0.85)
+  lgd.SetName('lgd' + tag)
+  lgd.SetBorderSize(0)
+  lgd.SetFillColor(0)
+  # Z measured
+  rawFF.SetBins(10, 0, 1.0)
+  DrawFF(rawFF, 'hRaw_' + tag)
+  lgd.AddEntry(rawFF, 'Raw')
   for nIter in range(4,5):
     bayes.SetIterations(nIter)
-    hist = bayes.Hreco(0)
-    hist.SetName('hBayes' + repr(nIter) + '_' + tag)
-    hist.Scale(1./hist.Integral('width'))
-    histColor = next(COLOR)
-    hist.SetLineColor(histColor)
-    hist.SetMarkerColor(histColor)
-    hist.SetMarkerStyle(next(MARKER))
-    hist.Draw('same PE')
+    hist = DrawFF(bayes.Hreco(0), 'hBayes' + repr(nIter) + '_' + tag)
+    lgd.AddEntry(hist, 'Bayes (N=%d)' % nIter)
+  lgd.Draw('same')
+  padFF.SaveAs('UnfoldFF_' + tag + '.root')
+  padFF.SaveAs('UnfoldFF_' + tag + '.pdf')
 
-padFF.cd(1)
 UnfoldFF(raw.hZPromptAfter, mc.hResponseFF_Prompt, 'Prompt')
-padFF.cd(2)
 UnfoldFF(raw.hZBdecayAfter, mc.hResponseFF_Bdecay, 'Bdecay')
 
-padFF.SaveAs('UnfoldFF.root')
 
 
 
--- Plot/ana_util.py
+++ Plot/ana_util.py
@@ -7,7 +7,21 @@ import ROOT
 from ROOT import kBlack, kRed, kBlue, kGreen, kOrange, kViolet, kCyan, kPink
 import sys, os, time, math, json, logging
 
+# Normalize by column
+  # X=measured, Y=true
+def ResponseNorm(h2):
+  NX = h2.GetNbinsX()
+  NY = h2.GetNbinsY()
+  for ix in range(1,NX+1):
+    SumY = h2.Integral(ix, ix, 1, NY)
+    if(SumY == 0):
+      continue
+    for iy in range(1, NY+1):
+      val = h2.GetBinContent(ix, iy) / SumY
+      h2.SetBinContent(ix, iy, val)
+  return h2
 # Input: TH1, Int_t
+# Deprecated: use TH1/2::Scale(1./TH::Integral('width'))
 def HistNorm(hist, NEv = 0):
   if(NEv == 0):
     NEv = hist.GetEntries()
@@ -34,7 +48,8 @@ def SelectMarker(MARKER_INDEX = 0):
   while(MARKER_INDEX < 100):
     yield MARKER_SET[MARKER_INDEX % len(MARKER_SET)]
     MARKER_INDEX += 1
-
+COLOR = SelectColor()
+MARKER = SelectMarker()
 
 if __name__ == '__main__':
   print("Utility lib for post-processing with ROOT")
