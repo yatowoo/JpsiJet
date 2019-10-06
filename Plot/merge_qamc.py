721fd42db4da4616d7807285549cffa0e30d10ab
ytwu <torrence@mail.ustc.edu.cn>
2019-10-06 10:30:22 +0800

    QM/MC - Update pT binning

--- Plot/merge_qamc.py
+++ Plot/merge_qamc.py
@@ -20,6 +20,7 @@ c.Draw()
 ana_util.PrintCover(c,args.print)
 
 PT_HARD_BINS = [12, 16, 21, 28, 36, 45, 57, 70, 85, 100, -1]
+BINNING_PT = ana_util.BINNING_JET_PT
 QA_NAME = ['VtxZ', 'ElePt','EleDCAxy','EleDCAz', 'JetPt', 'JetNtrk', 'TagJetPt', 'TagJetNtrk', 'JpsiPt', 'JpsiY']
 QA = list(range(len(PT_HARD_BINS) - 1))
 for i,pTmin in enumerate(PT_HARD_BINS[:-1]):
@@ -52,31 +53,31 @@ for i,pTmin in enumerate(PT_HARD_BINS[:-1]):
     # Track / Electron
   eleP = diele.FindObject('Track_ev1+')
   eleP.SetOwner(True)
-  QA[i]['ElePt'] = eleP.FindObject('Pt').Rebin(len(ana_util.BINNING_JPSI_PT)-1,'hElePt_%d' % i, ana_util.BINNING_JPSI_PT)
+  QA[i]['ElePt'] = eleP.FindObject('Pt').Rebin(len(BINNING_PT)-1,'hElePt_%d' % i, BINNING_PT)
   QA[i]['EleDCAxy'] = eleP.FindObject('dXY').Clone('hEleDXY_%d' % i)
   QA[i]['EleDCAxy'].GetXaxis().SetRangeUser(-2.,2.)
   QA[i]['EleDCAz'] = eleP.FindObject('dZ').Clone('hEleDZ_%d' % i)
   QA[i]['EleDCAz'].GetXaxis().SetRangeUser(-5.,5.)
   eleN = diele.FindObject('Track_ev1-')
   eleN.SetOwner(True)
-  QA[i]['ElePt'].Add(eleN.FindObject('Pt').Rebin(len(ana_util.BINNING_JPSI_PT)-1,'', ana_util.BINNING_JPSI_PT))
+  QA[i]['ElePt'].Add(eleN.FindObject('Pt').Rebin(len(BINNING_PT)-1,'', BINNING_PT))
   QA[i]['EleDCAxy'].Add(eleN.FindObject('dXY'))
   QA[i]['EleDCAz'].Add(eleN.FindObject('dZ'))
     # Jet
   jetQA = qa.FindObject('Jet_AKTChargedR040_tracks_pT0150_pt_scheme')
   jetQA.SetOwner(True)
   hs = jetQA.FindObject('jetVars')
-  QA[i]['JetPt'] = hs.Projection(0).Rebin(len(ana_util.BINNING_JET_PT)-1,'hJetPt_%d' % i, ana_util.BINNING_JET_PT)
-  QA[i]['JetNtrk'] = jetQA.FindObject('Ntracks_pT').ProfileX('hJetNtrk_%d' % i).Rebin(len(ana_util.BINNING_JET_PT)-1,'', ana_util.BINNING_JET_PT)
+  QA[i]['JetPt'] = hs.Projection(0).Rebin(len(BINNING_PT)-1,'hJetPt_%d' % i, BINNING_PT)
+  QA[i]['JetNtrk'] = jetQA.FindObject('Ntracks_pT').ProfileX('hJetNtrk_%d' % i).Rebin(len(BINNING_PT)-1,'', BINNING_PT)
   taggedJetQA = qa.FindObject('JpsiJet_AKTChargedR040_tracksWithPair_pT0150_pt_scheme')
   taggedJetQA.SetOwner(True)
   hs = taggedJetQA.FindObject('jetVars')
-  QA[i]['TagJetPt'] = hs.Projection(0).Rebin(len(ana_util.BINNING_JET_PT)-1,'hTagJetPt_%d' % i, ana_util.BINNING_JET_PT)
-  QA[i]['TagJetNtrk'] = taggedJetQA.FindObject('Ntracks_pT').ProfileX('hTagJetNtrk_%d' % i).Rebin(len(ana_util.BINNING_JET_PT)-1,'', ana_util.BINNING_JET_PT)
+  QA[i]['TagJetPt'] = hs.Projection(0).Rebin(len(BINNING_PT)-1,'hTagJetPt_%d' % i, BINNING_PT)
+  QA[i]['TagJetNtrk'] = taggedJetQA.FindObject('Ntracks_pT').ProfileX('hTagJetNtrk_%d' % i).Rebin(len(BINNING_PT)-1,'', BINNING_PT)
   # MC J/psi
   jpsiQA = mc.FindObject('JpsiBdecay')
   hs = jpsiQA.FindObject('jpsiVars')
-  QA[i]['JpsiPt'] = hs.Projection(0).Rebin(len(ana_util.BINNING_JPSI_PT)-1,'hJpsiPt_%d' % i, ana_util.BINNING_JPSI_PT)
+  QA[i]['JpsiPt'] = hs.Projection(0).Rebin(len(BINNING_PT)-1,'hJpsiPt_%d' % i, BINNING_PT)
   QA[i]['JpsiY'] = hs.Projection(1)
   QA[i]['JpsiY'].SetName('hJpsiY_%d' % i)
   # End
@@ -114,7 +115,7 @@ for hist in QA_NAME:
     c.SetLogy()
   else:
     c.SetLogy(False)
-    QA[0][hist].GetYaxis().SetRangeUser(0, 30)
+    QA[0][hist].GetYaxis().SetRangeUser(0, 15)
   c.Print(args.print, 'Title:' + hist)
   c.Write('c' + hist)
 
