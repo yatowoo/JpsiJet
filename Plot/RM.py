ca4b5ceba068cec5b1737020bb0a5516fe183b4f
ytwu <torrence@mail.ustc.edu.cn>
2019-10-07 20:15:36 +0800

    Plot/RM - Adapt to CMS plotting

--- Plot/RM.py
+++ Plot/RM.py
@@ -23,14 +23,16 @@ c.Divide(3,3,0,0)
 JET_PT_BINS = [10,15,25,100]
 RM = [[1,2,3],[4,5,6],[7,8,9]]
 PAD_INDEX = [7,4,1,8,5,2,9,6,3]
+PAVE = [[1,2,3],[4,5,6],[7,8,9]]
 response.GetAxis(0).SetRangeUser(0,1)
 response.GetAxis(1).SetRangeUser(0,1)
+
 for i in range(3):
-  response.GetAxis(3).SetRangeUser(JET_PT_BINS[i],JET_PT_BINS[i+1])
+  response.GetAxis(2).SetRangeUser(JET_PT_BINS[i],JET_PT_BINS[i+1])
   SUM = None
   for j in range(3):
-    response.GetAxis(2).SetRangeUser(JET_PT_BINS[j],JET_PT_BINS[j+1])
-    RM[i][j] = response.Projection(0,1)
+    response.GetAxis(3).SetRangeUser(JET_PT_BINS[j],JET_PT_BINS[j+1])
+    RM[i][j] = response.Projection(1,0)
     RM[i][j].SetName('RM_%d_%d' % (i, j))
     if(SUM is None):
       SUM = RM[i][j].Clone('hSum_%d' % i)
@@ -47,9 +49,22 @@ for i in range(3):
         val = RM[i][j].GetBinContent(ix, iy) / SumY
         RM[i][j].SetBinContent(ix, iy, val)
     c.cd(PAD_INDEX[3*i+j])
+    RM[i][j].SetTitle('')
     RM[i][j].SetMaximum(1.0)
     RM[i][j].SetMinimum(0.0)
     RM[i][j].Draw('COLZ')
+    PAVE[i][j] = ROOT.TPaveText(0.15,0.8,0.4,0.95,'brNDC')
+    PAVE[i][j].SetName('TxtCuts_%d_%d' % (i,j))
+    PAVE[i][j].SetFillColor(0)
+    if(i==2):
+      PAVE[i][j].AddText('p_{T,det} > %d GeV' % JET_PT_BINS[i])
+    else:
+      PAVE[i][j].AddText('%d < p_{T,det} < %d GeV' % (JET_PT_BINS[i],JET_PT_BINS[i+1]))
+    if(j==2):
+      PAVE[i][j].AddText('p_{T,gen} > %d GeV' % JET_PT_BINS[j])
+    else:
+      PAVE[i][j].AddText('%d < p_{T,gen} < %d GeV' % (JET_PT_BINS[j],JET_PT_BINS[j+1]))
+    PAVE[i][j].Draw('same')
 
 c.SaveAs('RM.root')
 c.SaveAs('RM.pdf')
