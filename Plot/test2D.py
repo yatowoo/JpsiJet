bef5da8e428f52d466b75465d0f6d230c6a19577
ytwu <torrence@mail.ustc.edu.cn>
2020-03-04 17:17:39 +0100

    Unfold - Test variable binning for TH2D

---
+++ Plot/test2D.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Test TH2D rebinning
+
+import ROOT
+from array import array
+
+f = ROOT.TFile("output/AnaMerge_Full_Train.root")
+
+# z vs pT,jet
+h2data = f.TagInfoH.Projection(3,5)
+
+# Binning for processing (raw, result and final)
+  # Low-edge
+BINNING_Z = [0.1*x for x in range(0,11,1)]
+NX = len(BINNING_PT) - 1
+BINNING_Z = array('d', BINNING_Z)
+
+BINNING_PT = list(range(10,20,2))
+BINNING_PT += list(range(20,40,5))
+NY = len(BINNING_Z) - 1
+BINNING_PT = array('d', BINNING_PT)
+# Histogram setup
+h2 = ROOT.TH2D("h2zpt","#it{z}(#it{p}_{T,J/#psi}/p_{T,jet}) vs #it{p}_{T,jet}", NX, BINNING_PT, NY, BINNING_Z)
+for i in range(1, NX+1, 1):
+  for j in range(1, NY+1, 1):
+    h2.Fill(BINNING_PT[i], BINNING_Z[j], i)
+
+h2.Draw("COLZ")
