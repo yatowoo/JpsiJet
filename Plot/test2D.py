6da21fc62782735bc8661d9e80d9f23816f2f4ea
ytwu <torrence@mail.ustc.edu.cn>
2020-03-05 12:19:09 +0100

    Unfold - Tesst 2D data filling

--- Plot/test2D.py
+++ Plot/test2D.py
@@ -9,6 +9,10 @@ f = ROOT.TFile("output/AnaMerge_Full_Train.root")
 
 # z vs pT,jet
 h2data = f.TagInfoH.Projection(3,5)
+NX_DATA = h2data.GetNbinsX()
+xdata = h2data.GetXaxis()
+NY_DATA = h2data.GetNbinsY()
+ydata = h2data.GetYaxis()
 
 # Binning for processing (raw, result and final)
   # Low-edge
@@ -22,8 +26,13 @@ NX = len(BINNING_PT) - 1
 BINNING_PT = array('d', BINNING_PT)
 # Histogram setup
 h2 = ROOT.TH2D("h2zpt","#it{z}(#it{p}_{T,J/#psi}/p_{T,jet}) vs #it{p}_{T,jet}", NX, BINNING_PT, NY, BINNING_Z)
-for i in range(0, NX, 1):
-  for j in range(0, NY, 1):
-    h2.Fill(BINNING_PT[i], BINNING_Z[j], i*i + j*j)
+for i in range(1, NX_DATA+1, 1):
+  for j in range(1, NY_DATA+1, 1):
+    h2.Fill(xdata.GetBinCenter(i), ydata.GetBinCenter(j), h2data.GetBinContent(i,j))
 
+c = ROOT.TCanvas('c1','FF',1600,800)
+c.Divide(2)
+c.cd(1)
+h2data.Draw("COLZ")
+c.cd(2)
 h2.Draw("COLZ")
